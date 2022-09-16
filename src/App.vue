628aa32a00d3191ffec6885d436925e7c25a25e4
dabiko <dabiko.blaise@gmail.com>
2022-09-16 15:59:31 +0100

    syncfusion license

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import RightSidebar from './components/RightSidebar.vue';
+
 // Registering Syncfusion license key
 import { registerLicense } from '@syncfusion/ej2-base';
+// import * as dotenv from 'dotenv'
+// dotenv.config()
 
-registerLicense('ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkxiX39Wc3BRQGFdU0M=');
+registerLicense("ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkxiX39Wc3BRQGFdU0M=");
 
 
 </script>
